6b43576fd75e32f3e2b635a123afa7af557a5b5e
Irvi3ngg <irvi3nggasimov@gmail.com>
2023-02-05 12:17:34 -0600

    Correct indentation problems on .../ICEP_robotica/move.py

--- server/ICEP_robotica/move.py
+++ server/ICEP_robotica/move.py
@@ -140,23 +140,23 @@ def destroy():
 if __name__ == '__main__':
 	try:
 		speed_set_forward = 60
-                speed_set_turn = 80
+		speed_set_turn = 80
 		setup()
-
-                time.sleep(20) #Delay time for disconnecting eth cable :c
+		
+		time.sleep(20) #Delay time for disconnecting eth cable :c
 		move(speed_set_forward, 'forward', 'no', 0.8)
 		time.sleep(0.5)
 		motorStop()
-
-                time.sleep(1)
-                move(speed_set_turn, 'no', 'right', 0.8)
-                time.sleep(1.2)
-                motorStop()
-
-                time.sleep(1)
-                move(speed_set_turn, 'no', 'left', 0.8)
-                time.sleep(1.2)
-                motorStop()
+		
+		time.sleep(1)
+		move(speed_set_turn, 'no', 'right', 0.8)
+		time.sleep(1.2)
+		motorStop()
+		
+		time.sleep(1)
+		move(speed_set_turn, 'no', 'left', 0.8)
+		time.sleep(1.2)
+		motorStop()
 
 		destroy()
 	except KeyboardInterrupt:

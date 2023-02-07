39667608a21e218359c23690aa5f34d8f1773ed3
Irvi3ngg <irvi3nggasimov@gmail.com>
2023-02-07 21:55:47 +0000

    Develope avoid.py functions

---
+++ server/ICEP_robotica/avoid.py
@@ -0,0 +1,31 @@
+# File name: avoid.py
+# Description: Detect obstacles and avoid collisions
+# Author: Irving (irvi3nggasimov@gmail.com)
+# Date: 2023/02/07
+
+import time, ultra_ICEP, move
+"Set speed"
+speed_set_forward = 60
+speed_set_turn = 80
+
+"Create function to avoid obstacles"
+def avoid():
+time.sleep(20) # Delay time to disconect eth cable
+    while True:
+        distance = ultra_ICEP.checkdist()*100 # Measure distance
+        time.sleep(0.4)
+
+        if distance > 0 and distance > 25:   # Move forward as long as distance > 25cm
+            move.move(speed_set_forward, 'forward', 'no', 0.8)
+        elif distance > 0 and distance <= 25: # Stop when distance <= 25cm
+            move.motorStop()
+            time.sleep(0.3)
+            while distance > 0 and distance <= 25:
+                move(speed_set_turn, 'no', 'right', 0.8) # Move robot to the left 90deg
+                time.sleep(1)
+                move.motorStop()
+                distance = ultra_ICEP.checkdist()*100 # Measure distance
+                time.sleep(0.3)
+
+if __name__ == '__main__':
+    ul

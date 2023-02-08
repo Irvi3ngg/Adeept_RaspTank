0606e4f9f4adebb5d2e86a2d3ae7ebee456962de
Irvi3ngg <irvi3nggasimov@gmail.com>
2023-02-08 14:45:00 -0600

    Modify avoid.py to identify and escape from closed roads

--- server/ICEP_robotica/avoid.py
+++ server/ICEP_robotica/avoid.py
@@ -14,17 +14,32 @@ def avoid():
         distance = ultra_ICEP.checkdist()*100 # Measure distance
         time.sleep(0.1)
 
-        if distance > 0 and distance > 25:   # Move forward as long as distance > 25cm
+        if distance > 25:   # Move forward as long as distance > 25cm
             move.move(speed_set_forward, 'forward', 'no', 0.8)
+
         elif distance > 0 and distance <= 25: # Stop when distance <= 25cm
             move.motorStop()
-            time.sleep(0.3)
-            while distance > 0 and distance <= 25:
-                move.move(speed_set_turn, 'no', 'right', 0.8) # Move robot to the left 90deg
-                time.sleep(1)
+            time.sleep(0.2)
+            move.move(speed_set_turn, 'no', 'right', 0.8) # Move robot to the left 90deg
+            time.sleep(1)
+            move.motorStop()
+            distance = ultra_ICEP.checkdist()*100 # Measure distance
+            time.sleep(0.1)
+
+            "If distance on the left is < 25cm, check distance on the right side"
+            if distance > 0 and distance <= 25:
+                move.move(speed_set_turn, 'no', 'left', 0.8)
+                time.sleep(2) # Rotate 180deg
                 move.motorStop()
-                distance = ultra_ICEP.checkdist()*100 # Measure distance
+                distance = ultra_ICEP.checkdist()*100
                 time.sleep(0.1)
+                
+                if distance > 0 and distance <= 25 : # There is a closed area, come back
+                    move.move(speed_set_turn, 'no', 'left', 0.8)
+                    time.sleep(1)
+                    move.motorStop()
+                    time.sleep(0.2)
+
 
 if __name__ == '__main__':
     ultra_ICEP.setup()

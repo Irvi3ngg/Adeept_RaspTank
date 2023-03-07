df1b4c8551b4d6d7c1adb658ebf3caeee8a249bb
Irvi3ngg <irvi3nggasimov@gmail.com>
2023-03-07 17:03:10 -0600

    Add avoid_2.py, second version of avoid

---
+++ server/ICEP_robotica/avoid_2.py
@@ -0,0 +1,52 @@
+'''
+File name: avoid_2.py
+Description: Detect obstacles according to competition
+Author: Irving Juarez
+Date: 6/Mar/2023
+'''
+
+import time, ultra_ICEP, move
+
+"Set speed"
+speed_forward = 60
+speed_turn = 100
+
+"Setup function"
+def setuplibs():
+    ultra_ICEP.setup()
+    move.setup()
+
+"Constants"
+right = 1
+left = 0
+turns = {0: 'right', 1: 'right', 2: 'left', 3: 'left'}
+cont = 0
+dist = 20
+
+"Create function to avoid"
+def avoid():
+    while True:
+        distance = ultra_ICEP.checkdist()*100
+        time.sleep(0.1)
+
+        if distance > dist:
+            move.move(speed_forward, 'forward', 'no', 0.8)
+        
+        elif distance <= dist:
+            move.motorStop()
+            direct = turns[cont]
+            time.sleep(0.4)
+            while distance <= dist:
+                move.move(speed_turn, 'no', direct, 0.8)
+                distance = ultra_ICEP.checkdist()*100
+                time.sleep(0.1)
+            cont += 1
+
+if __name__ == "__main__":
+    try:
+        setuplibs()
+        time.sleep(4)
+        avoid()
+        move.destroy()
+    except KeyboardInterrupt:
+        move.destroy()
